681a96a8280e31ccaab4a35de6fadf542ca2a3b1
Andrew Huntington <andrew.huntington@gmail.com>
2019-09-07 15:49:50 +0900

    Movement
    Added basic player movement with PlayerMovement.cs

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal"); //GetAxisRaw only returns the values of 0, -1, 1. 
+        change.y = Input.GetAxisRaw("Vertical");   //Good for digital movement (vs. GetAxis)
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + change.normalized * speed * Time.deltaTime);
+    }
+
+}
